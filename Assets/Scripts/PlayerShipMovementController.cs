571ee9acc1d3d70b2d65b8ec7da5ba8ae13d01a0
Dragon <43445186+ccpdragon@users.noreply.github.com>
2018-09-20 18:36:57 +0000

    Fix some shtuff

--- Assets/Scripts/PlayerShipMovementController.cs
+++ Assets/Scripts/PlayerShipMovementController.cs
@@ -7,8 +7,8 @@ public class PlayerShipMovementController : MonoBehaviour
     public Rigidbody2D rb;
     private Vector2 directionInput;
 
-    public float agility = 0.5f;
-    public float maxSpeed = 10;
+    public float agility;
+    public float maxSpeed;
 
     // Start is called before the first frame update
     private void Start()
@@ -19,17 +19,64 @@ public class PlayerShipMovementController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        // Get the player's desired direction
         directionInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-
-        // thrustInput = Input.GetAxis("Vertical");
-        // turnInput = Input.GetAxis("Horizontal");
     }
 
     void FixedUpdate()
     {
+        // Get a rotation based on the desired direction
+        float desiredDirection = DetermineDesiredRotation();
+
+        // Check to see if the ship is pointed in the player's desired direction
+        if (!Mathf.Approximately(rb.rotation, desiredDirection))
+        {
+            // Find the delta angle from current direction to desired direction
+            float deltaRotation = desiredDirection - rb.rotation;
+
+            // Check to see if the desired direction is further than the ship can rotate this update
+            if (Mathf.Abs(deltaRotation) > agility)
+            {
+                // Rotate the ship based on its agility
+                // But first check to see which direction we have to go
+                rb.MoveRotation(rb.rotation + Mathf.Sign(deltaRotation) * agility);
+
+                /* if (deltaRotation > 0)
+                {
+                    rb.MoveRotation(rb.rotation + agility);
+                }
+                else
+                {
+                    rb.MoveRotation(rb.rotation - agility);
+                } */
+            }
+            else
+            {
+                // Rotate the ship to the specified direction
+                rb.MoveRotation(desiredDirection);
+            }
+        }
+
         print(rb.rotation);
-        print(directionInput.rotation);
-//        rb.AddTorque (rb.velocity, directionInput * maxSpeed, agility);
-        
+        print(desiredDirection);
+    }
+
+    // How far does the ship need to rotate
+    private float DetermineDesiredRotation()
+    {
+        if (directionInput != Vector2.zero)
+        {
+            // Find out how far the ship needs to rotate to match the direction the player wants to go
+            float desiredDirection = Vector2.SignedAngle(Vector2.up, directionInput);
+
+            if (desiredDirection < 0)
+            {
+                desiredDirection += 360;
+            }
+
+            return desiredDirection;
+        }
+
+        return rb.rotation;
     }
 }
