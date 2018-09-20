88602786c5c0dcd0f0a1f147cb3d3d50a9ad4635
Dragon <43445186+ccpdragon@users.noreply.github.com>
2018-09-20 14:12:47 +0000

    Added DroneMace Scene and Player Ship Controller Script

---
+++ Assets/Scripts/PlayerShipMovementController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShipMovementController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float maxVelocity;
+    public float maxThrust;
+    public float turnAgility;
+    private float thrustInput;
+    private float turnInput;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        thrustInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddRelativeForce(Vector2.up * thrustInput, ForceMode2D.Impulse);
+
+        if (rb.angularVelocity < turnAgility)
+        {
+            rb.AddTorque(turnInput * -5.0f);
+        }
+    }
+}
