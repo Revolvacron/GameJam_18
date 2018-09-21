9eb12c891c6c31ca38d27ad80915aeb4aee66e54
Dragon <43445186+ccpdragon@users.noreply.github.com>
2018-09-21 13:56:22 +0000

    Revert "Merge branch 'master' of https://github.com/Revolvacron/GameJam_18"
    This reverts commit fd3d97918570d9fb636b734e5902fd078efdbd98, reversing
    changes made to 1b6b88ff1dce370187db0a32ff0d5b4a43b0623a.

--- Assets/Scripts/PlanetOrbit.cs
+++ Assets/Scripts/PlanetOrbit.cs
@@ -2,18 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlanetOrbit : MonoBehaviour
+public class Orbit : MonoBehaviour
 {
     public float orbitSpeed;
-    public Vector3 orbitDirection;
-    public float rotationSpeed;
+    public float orbitDirection;
 
     // Update is called once per frame
     void Update()
     {
-        //Rotate the planet about its own z-axis
-        transform.Rotate(axis: Vector3.forward, angle: Time.deltaTime*rotationSpeed);
-        //Rotate the planet around the world's z-axis
-        transform.RotateAround(Vector3.zero, orbitDirection, orbitSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right, Time.deltaTime*5f);
     }
 }
