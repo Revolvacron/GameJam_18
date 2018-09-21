2ae53fc14ffcc86336265d8c71e323e2c0b5bd9c
Revolvacron <investinamule@gmail.com>
2018-09-21 03:51:36 +0000

    Trying to get orbits working

--- Assets/Scripts/PlanetOrbit.cs
+++ Assets/Scripts/PlanetOrbit.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Orbit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float orbitSpeed;
+    public float orbitDirection;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.right, Time.deltaTime*5f);
     }
 }
