48d6f359dfe33e91cd83292345dde1a7da066a48
Revolvacron <investinamule@gmail.com>
2018-09-21 16:02:50 +0000

    Implemented kill boundary

---
+++ Assets/Scripts/BoundaryDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryDestroy : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("asset in game area");
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Collison occurs");
+        Destroy(other.gameObject);
+    }
+}
